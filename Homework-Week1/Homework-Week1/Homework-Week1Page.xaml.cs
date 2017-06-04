6a8850789f2c59abe1c91d6b91e36364bbea3601
Abraham Isaac Durán <aid.mest@gmail.com>
2017-06-04 00:21:42 -0400

    Add the project for the first week's homework

---
+++ Homework-Week1/Homework-Week1/Homework-Week1Page.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace HomeworkWeek1
+{
+    public partial class Homework_Week1Page : ContentPage
+    {
+        public Homework_Week1Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
