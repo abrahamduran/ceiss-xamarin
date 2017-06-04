7e77fbcb902b8b549ff3eb861a9954559a8d78af
Abraham Isaac Durán <aid.mest@gmail.com>
2017-06-04 08:55:45 -0400

    Relocate projects in the working directory.

---
+++ Week 1/Homework-1/Homework-1/DetailPage.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Homework1
+{
+    public class DetailPage : ContentPage
+    {
+        public DetailPage(String title, Color backgroundColor)
+        {
+            Title = "Result";
+
+            var label = new Label
+            {
+                Text = title,
+                TextColor = Color.White,
+				HorizontalOptions = LayoutOptions.FillAndExpand,
+                WidthRequest = Application.Current.MainPage.Width,
+                HorizontalTextAlignment = TextAlignment.Center
+            };
+
+            var layout = new RelativeLayout
+            {
+                BackgroundColor = backgroundColor,
+                Children = {
+                    {
+                        label,
+                        Constraint.Constant(0),
+                        Constraint.RelativeToParent(x => x.Height / 2)
+                    }
+                }
+            };
+
+            Content = layout;
+        }
+    }
+}
+
---
+++ Week 1/Homework-1/Homework-1/MyPage.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace Homework1
+{
+    public class MyPage : ContentPage
+    {
+        Label label;
+        Entry entry;
+
+        public MyPage()
+        {
+            label = new Label();
+            entry = new Entry();
+
+            var redButton = GetButton("Red", Color.Red);
+            var blueButton = GetButton("Blue", Color.Blue);
+            var greenButton = GetButton("Green", Color.Green);
+
+            var buttons = new StackLayout()
+            {
+                Orientation = StackOrientation.Horizontal
+            };
+            buttons.Children.Add(redButton);
+            buttons.Children.Add(blueButton);
+            buttons.Children.Add(greenButton);
+
+            var layout = new StackLayout()
+            {
+                Margin = 15,
+                VerticalOptions = LayoutOptions.Center
+            };
+            layout.Children.Add(entry);
+            layout.Children.Add(buttons);
+            layout.Children.Add(label);
+
+            Content = layout;
+        }
+
+        Button GetButton(String text, Color color)
+        {
+            var button = new Button()
+            {
+                Text = text,
+                TextColor = Color.White,
+                BackgroundColor = color,
+                HorizontalOptions = LayoutOptions.FillAndExpand
+            };
+
+            button.Clicked += ClickHandler;
+
+            return button;
+        }
+
+        void ClickHandler(Object sender, EventArgs e) 
+        {
+			var title = "unknow color";
+			var color = ((Button)sender).BackgroundColor;
+
+            if (color == Color.Red) title = "Red";
+            else if (color == Color.Blue) title = "Blue";
+            else if (color == Color.Green) title = "Green";
+
+            label.Text = entry.Text;
+            label.TextColor = color;
+
+            var detailPage = new DetailPage(title, color);
+
+            Navigation.PushAsync(detailPage);
+        }
+    }
+}
